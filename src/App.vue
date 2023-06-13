1799b7aade7af9d08f6199b4f1f5a61127f036bc
xlearns <2447684095@qq.com>
2023-06-13 09:50:15 +0800

    feat: add readme feature

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,16 @@ import useGlobal from "@/models/global";
 const msg = ref("hello world");
 const { list, useList, haha, useHaha } = useGlobal();
 setTimeout(() => {
-  useList("2023");
-  useHaha((item) => {
-    item.push(4);
-    return item;
-  });
+	useList("2023");
+	useHaha((item) => {
+		item.push(4);
+		return item;
+	});
 }, 2000);
 </script>
 
 <template>
-  {{ msg }}
-  <Test />
-  <p>app: {{ list }} {{ haha }}</p>
+	{{ msg }}
+	<Test />
+	<p>app: {{ list }} {{ haha }}</p>
 </template>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,12 @@
-const global = {
-  list: "hello",
-  haha: [1, 2, 3],
-  test: {},
+interface IGlobal {
+	list: string;
+	haha: number[];
+	test: any;
+}
+const global: IGlobal = {
+	list: "hello",
+	haha: [1, 2, 3],
+	test: null,
 };
 
 export default global;
